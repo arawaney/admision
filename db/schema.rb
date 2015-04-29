4c963e2b7a4a70072fa6e8031bd3dd0dca7cec14
arawaney <arawaneystartup@gmail.com>
2015-04-29 17:36:31 -0430

    modificaciones en vistas

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150402212117) do
+ActiveRecord::Schema.define(version: 20150422205747) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "name"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20150402212117) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "role_id"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
